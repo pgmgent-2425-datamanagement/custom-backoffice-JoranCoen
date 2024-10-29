ef176e58c57f949b96f12277fea19c98b68a7fc2
pgm-jorancoen <joran.coen@student.arteveldehs.be>
2024-10-29 23:34:49 +0100

    fixes css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
+module.exports = {
+  content: [
+    './views/**/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+}
+
